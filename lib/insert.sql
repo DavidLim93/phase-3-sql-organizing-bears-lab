4f53716750bf301c4c59a17973690d8f7b0adb10
David <lim.david93@gmail.com>
2022-12-12 13:50:43 +1100

    Complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, "Mr.Chocolate", 20, "M", "dark brown", "calm", 1);
+INSERT INTO bears VALUES (2, "Rowdy", 25, "M", "blue", "energetic", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 19, "F", "purple", "quiet", 1);
+INSERT INTO bears VALUES (4, "Seargent Brown", 31, "M", "black", "loud", 1);
+INSERT INTO bears VALUES (5, "Melissa", 60, "F", "pink", "clam", 0);
+INSERT INTO bears VALUES (6, "Grinch", 53, "F", "green", "evil", 1);
+INSERT INTO bears VALUES (7, "Wendy", 30, "F", "violet", "nice", 0);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "aggressive", 0);
